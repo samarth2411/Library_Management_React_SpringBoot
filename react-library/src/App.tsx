4ef0c03660bddc4eb0146c50ce3314ca0a1e8e4d
samarth2411 <samarthkporwl@gmail.com>
2023-02-14 22:40:06 +0530

    created Explore Top Books Component

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { ExploreTopBooks } from './layout/NavbarAndFooter/HomePage/ExploreTopBooks';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+    </div>
+    
+
   );
 }
 
