39072efb311aca7b333e06aa41b9e458d9ea4896
samarth2411 <samarthkporwl@gmail.com>
2023-02-20 22:39:44 +0530

    created carousel component

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Carousel } from './layout/NavbarAndFooter/HomePage/Carousel';
 import { ExploreTopBooks } from './layout/NavbarAndFooter/HomePage/ExploreTopBooks';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <ExploreTopBooks/>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
     </div>
-    
+
 
   );
 }
